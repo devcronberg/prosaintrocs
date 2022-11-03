e70b380383e1f5975a21d79c767d3aedb8017b72
Michell Cronberg <michell@cronberg.dk>
2022-11-03 18:49:04 +0100

    .

---
+++ App5/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Start");
+// try + tab
+try
+{
+    string txt = System.IO.File.ReadAllText("C:\\prosaintrocs\\test.txt");
+    System.Console.WriteLine(txt);
+}
+catch (System.Exception ex)
+{
+    System.Console.WriteLine("Fejl " + ex.Message);
+}
+System.Console.WriteLine("Slut");
